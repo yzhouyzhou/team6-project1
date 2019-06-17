98baba5e16ce2379e032205e142633e011646d22
Ying Zhou <yzhouyzhou@yahoo.com>
2019-06-16 19:23:25 -0500

    update maps

--- assets/javascripts/maps.js
+++ assets/javascripts/maps.js
@@ -35,6 +35,7 @@ var locations = [
     { lat: 41.879540, lng: -87.624040 }, // arts institute
     { lat: 41.885440, lng: -87.627260 },  // chicago theatre
     { lat: 41.888280, lng: -87.632500 },  // house of blue
+    { lat: 41.884315, lng: -87.620542 },  // Jay Pritzker Pavilion Millennium Park
     { lat: 41.891710, lng: -87.609660 }, // navy pier
     { lat: 42.0449188, lng: -87.6808331 }, //600 Emerson Evanston  
 ]
