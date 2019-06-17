0972f1da41ec5c36894bda95d40fe919f0853dea
Ying Zhou <yzhouyzhou@yahoo.com>
2019-06-16 19:07:51 -0500

    integrate google map with marker in

--- assets/javascripts/maps.js
+++ assets/javascripts/maps.js
@@ -1,18 +1,12 @@
 var marker;
-var latitude = 41.7697383;
-var longitude = -88.1517823;
-var btn = $("<button>");
-btn.html("Location");
-btn.attr("latitude-value", "41.8781");
-btn.attr("longitude-value", "-87.6298");
+var latitude = 41.885440;
+var longitude = -87.627260;
 var aMap = document.getElementById('map');
-var m = $("#map");
-$("#location-display").append(btn);
 
 function initMap() {
 
     var map = new google.maps.Map(aMap, {
-        zoom: 9,
+        zoom: 14,
         center: { lat: latitude, lng: longitude }
     });
 
@@ -35,21 +29,12 @@ function initMap() {
     var markerCluster = new MarkerClusterer(map, markers,
         { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
-    btn.on("click", function () {
-        var newLat = btn.attr("latitude-value");
-        var newLng = btn.attr("longitude-value");
-        console.log(newLat, newLng);
-        var center = new google.maps.LatLng(newLat, newLng);
-        map.panTo(center);
-        // marker.DROP(center);
-    })
-
 }
 var locations = [
-    { lat: 41.7697383, lng: -88.1517823 },
-    { lat: 41.8781, lng: -87.6298 },
-    { lat: 41.5677383, lng: -88.1517823 },
-    { lat: 41.7667383, lng: -88.4517823 }
+    { lat: 41.878910, lng: -87.635490 },  // willis tower
+    { lat: 41.879540, lng: -87.624040 }, // arts institute
+    { lat: 41.885440, lng: -87.627260 },  // chicago theatre
+    { lat: 41.888280, lng: -87.632500 },  // house of blue
+    { lat: 41.891710, lng: -87.609660 }, // navy pier
+    { lat: 42.0449188, lng: -87.6808331 }, //600 Emerson Evanston  
 ]
-
-
